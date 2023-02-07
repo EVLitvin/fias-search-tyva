33840d18b03008775e10b0e26f48141dcbb3c287
Evgenii Litvin <ev_litvin@yahoo.com>
2023-02-07 13:21:14 +0500

    Init commit.

---
+++ src/main/java/com/example/fias_search_tyva/FiasSearchTyvaApplication.java
@@ -0,0 +1,13 @@
+package com.example.fias_search_tyva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiasSearchTyvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiasSearchTyvaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fias_search_tyva/FiasSearchTyvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fias_search_tyva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiasSearchTyvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
