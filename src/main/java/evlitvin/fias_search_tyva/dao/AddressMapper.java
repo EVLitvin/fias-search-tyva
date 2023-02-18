34da1244a45ba2787906a58ea4d0bf4833e39485
Evgenii Litvin <ev_litvin@yahoo.com>
2023-02-18 16:37:23 +0500

    Make changes.

--- src/main/java/evlitvin/fias_search_tyva/dao/AddressMapper.java
+++ src/main/java/evlitvin/fias_search_tyva/dao/AddressMapper.java
@@ -13,15 +13,15 @@ public class AddressMapper implements RowMapper<Address> {
     public Address mapRow(ResultSet rs, int rowNum) throws SQLException {
         Address address = new Address();
         address.setAddress(rs.getString("address"));
-//        address.setObjectType(rs.getString("objectType"));
-//        address.setObjectName(rs.getString("objectName"));
-//        address.setObjectId(rs.getLong("objectId"));
-//        address.setObjectGUID(rs.getString("objectGUID"));
-//        address.setObjectParent(rs.getLong("objectParent"));
-//        address.setObjectActiveStatus(rs.getByte("objectActiveStatus"));
-//        address.setObjectActualStatus(rs.getByte("objectActualStatus"));
-//        address.setObjectLevel(rs.getByte("objectLevel"));
-//        address.setPathToObject(rs.getString("pathToObject"));
+        address.setObjectType(rs.getString("objectType"));
+        address.setObjectName(rs.getString("objectName"));
+        address.setObjectId(rs.getLong("objectId"));
+        address.setObjectGUID(rs.getString("objectGUID"));
+        address.setObjectParent(rs.getLong("objectParent"));
+        address.setObjectActiveStatus(rs.getByte("objectActiveStatus"));
+        address.setObjectActualStatus(rs.getByte("objectActualStatus"));
+        address.setObjectLevel(rs.getByte("objectLevel"));
+        address.setPathToObject(rs.getString("pathToObject"));
         return address;
     }
 }
--- src/main/java/evlitvin/fias_search_tyva/entity/Address.java
+++ src/main/java/evlitvin/fias_search_tyva/entity/Address.java
@@ -1,24 +1,25 @@
 package evlitvin.fias_search_tyva.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Data
+@Setter
+@Getter
+@EqualsAndHashCode
 @NoArgsConstructor
+
 @AllArgsConstructor
 public class Address {
 
     private String address;
-//    private String objectType;
-//    private String objectName;
-//    private Long objectId;
-//    private String objectGUID;
-//    private Long objectParent;
-//    private Byte objectActiveStatus;
-//    private Byte objectActualStatus;
-//    private Byte objectLevel;
-//    private String pathToObject;
+    private String objectType;
+    private String objectName;
+    private Long objectId;
+    private String objectGUID;
+    private Long objectParent;
+    private Byte objectActiveStatus;
+    private Byte objectActualStatus;
+    private Byte objectLevel;
+    private String pathToObject;
 }
